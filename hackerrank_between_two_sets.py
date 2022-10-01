7760156664e74a348f3cb843228404a283c4a95b
Akeem <akeem.mighty@gmail.com>
2022-10-01 09:02:49 -0400

    Completed Number Between Sets Hackerrank Challenge

--- hackerrank_between_two_sets.py
+++ hackerrank_between_two_sets.py
@@ -22,33 +22,41 @@ def getTotalX(a, b):
     startPoint = max(a)
     endPoint = min(b)
     arrayOfFactors = []
+    removed_list = []
  
     for num in range(startPoint, endPoint + 1):
         arrayOfFactors.append(num)
     for tempNum in tempA:
         for currentNum in arrayOfFactors:
             if currentNum % tempNum != 0:
-                print(f"{tempNum}%{currentNum} = {tempNum % currentNum}")
-                if currentNum in arrayOfFactors:
-                    arrayOfFactors.remove(currentNum)
-    print(arrayOfFactors)
+                removed_list.append(currentNum)
+    
     
     for tempNum in tempB:
         for currentNum in arrayOfFactors:
-            # print(f"{tempNum}%{currentNum} = {tempNum % currentNum}")
             if tempNum % currentNum !=0:
-                if currentNum in arrayOfFactors:
-                    arrayOfFactors.remove(currentNum)
-    print(arrayOfFactors)
+                removed_list.append(currentNum)
+
+    removed_list = list(set(removed_list))
+    
+    arrayOfFactors = [x for x in arrayOfFactors if x not in removed_list]
     return len(arrayOfFactors)
 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-# a = [2, 4]
-# b = [16, 32, 96]
-# num = getTotalX(a, b)
-# print(num)
+    first_multiple_input = input().rstrip().split()
 
-a = [3, 4]
-b = [24, 48]
-num = getTotalX(a, b)
-print(num)
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
